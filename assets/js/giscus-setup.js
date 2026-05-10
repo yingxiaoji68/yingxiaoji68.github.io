0b6615bd1ad6a12b61ac61150b656b7faab5bf28
yingxiaoji68 <yingxiaoji68@gmail.com>
2026-05-10 02:59:49 +0000

    Deploying to gh-pages from @ yingxiaoji68/yingxiaoji68.github.io@5afd09d822cf33867c3640bd415b782fc1cdbed2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Yingxiaoji68/yixingji.github.io",
+    "data-repo": "yingxiaoji68/yingxiaoji68.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
